d3269e30cd28d148207424ba1932ae6daf07445e
tmc42 <tmclean1212@gmail.com>
2025-08-09 15:18:10 -0500

    Outlining logic and deps

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,14 @@
 // Main server side logic
 // VetRecDX 2025
 
+import (
+	"net/http"
+	"crypto/tls" // Want to support local and cloud based implementations so https will be necessary
+)
+
+
 func main() {
-    // 
+    // Handle
+	// Route
+	// Listen
 }
